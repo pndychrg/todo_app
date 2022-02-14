d0ae703230102c0ac8279e68b915d4cc7d7aa215
Chirag Pandey <pndy.chrg2003@gmail.com>
2022-02-14 22:27:38 +0530

    Edited Drawer Background Color

--- lib/helpers/drawer_navigation.dart
+++ lib/helpers/drawer_navigation.dart
@@ -38,10 +38,13 @@ class _DrawerNavigationState extends State<DrawerNavigation> {
                       ))),
           child: Card(
             child: ListTile(
-              leading: Icon(Icons.arrow_right),
+              leading: Icon(
+                Icons.arrow_right,
+              ),
               title: Text(
                 category['name'],
-                style: GoogleFonts.lato(fontSize: 20),
+                style:
+                    GoogleFonts.lato(fontSize: 20, fontWeight: FontWeight.w400),
               ),
             ),
           ),
@@ -50,11 +53,26 @@ class _DrawerNavigationState extends State<DrawerNavigation> {
     });
   }
 
+//creating a function to return gradient color
+  List<Color> _getGradientColor(BuildContext context) {
+    var _grad_color =
+        MediaQuery.of(context).platformBrightness == Brightness.light
+            ? [Colors.white10, Colors.lightBlue]
+            : [Colors.blueGrey.shade900.withOpacity(0.7), Colors.black12];
+    return _grad_color;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Drawer(
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+    return Drawer(
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: _getGradientColor(context),
+          ),
+        ),
         child: ListView(
           children: <Widget>[
             Card(
@@ -63,7 +81,9 @@ class _DrawerNavigationState extends State<DrawerNavigation> {
                   ListTile(
                     title: Text(
                       "Home",
-                      style: TextStyle(fontSize: 20),
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
                     ),
                     leading: Icon(
                       Icons.home,
@@ -77,7 +97,9 @@ class _DrawerNavigationState extends State<DrawerNavigation> {
                   ListTile(
                     title: Text(
                       "Categories",
-                      style: TextStyle(fontSize: 20),
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
                     ),
                     leading: Icon(
                       Icons.view_list,
--- lib/theme.dart
+++ lib/theme.dart
@@ -4,6 +4,7 @@ import 'package:todo_app/constants.dart';
 ThemeData lightThemeData(BuildContext context) {
   return ThemeData.light().copyWith(
     scaffoldBackgroundColor: kprimarycolor,
+    iconTheme: IconThemeData(color: Colors.white),
   );
 }
 
